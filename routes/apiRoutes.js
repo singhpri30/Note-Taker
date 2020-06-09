830f4f492832dd00214ba8225e0ca12676f8efc4
Pri_030 <priyankasingh30@gmail.com>
2020-06-09 11:34:50 -0400

    created html route and added it into server.js file

---
+++ routes/apiRoutes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+
+
+router.get("/", (req, res) => {
+
+    console.log("this is home page")
+
+
+})
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const PORT = 8080;
+
+//const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require("./routes/htmlRoutes");
+
+// middleware that is needed to use POST data
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.use('/', htmlRoutes);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running at ${PORT}`);
+})

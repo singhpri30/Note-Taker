551298d713ea0e66978ee6171868d8b90347c566
Pri_030 <priyankasingh30@gmail.com>
2020-06-12 17:53:54 -0400

    update api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,11 +1,37 @@
 const express = require("express");
 const router = express.Router();
+const fs = require("fs");
+
+let noteData = [];
 
 
-router.get("/", (req, res) => {
+const test = fs.readFileSync("./db/db.json", "utf8");
+//console.log(test);
 
-    console.log("this is home page")
+router.get("/notes", (req, res) => {
 
+    res.sendFile(path.join(__dirname, "..", "db", "db.json"));
+
+    // fs.readFile("./db/db.json", 'utf8', (err, noteData) => {
+    //     if (err) {
+    //         return console.log(err);
+    //     }
+    //     console.data(noteData)
+    //     res.send(noteData);
+    // });
+
+
+})
+
+router.post("/notes", (req, res) => {
+    var newNoteData = req.body;
+
+    fs.writeFile("./db/db.json", JSON.stringify(newNoteData), (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+    });
+    noteData.push(newNoteData);
+    res.json(newNoteData);
 
 })
 
--- server.js
+++ server.js
@@ -1,29 +1,21 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const PORT = 8080;
 
-//const apiRoutes = require('./routes/apiRoutes');
+const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require("./routes/htmlRoutes");
 
 // middleware that is needed to use POST data
+app.use(cors());
+app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-app.use('/', htmlRoutes);
-app.use('/notes', htmlRoutes);
-
-
-
-
-
-
-
-
-
-
-
-
+app.use("/", htmlRoutes);
+app.use("/notes", htmlRoutes);
+app.use("/api", apiRoutes);
 
 
 
